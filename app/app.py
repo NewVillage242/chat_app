35d774ae87a489e16f35042d8a7d21bc71c3d32e
twei <tingwei109@gmail>
2024-08-22 02:58:52 +0800

    Move messages route to separated files

--- app/app.py
+++ app/app.py
@@ -2,39 +2,13 @@ import time
 
 import redis
 from flask import Flask, render_template, request, redirect
+from messages.routes import mess_bp
 import os
 import time
 
 app = Flask(__name__)
+app.register_blueprint(mess_bp, url_prefix='/messages')
 cache = redis.Redis(host='redis', port=6379)
-
-# mySQL
-import mysql.connector
-db_config = {
-    'user': 'root',
-    'password': 'root',
-    'host': 'mysql',  # Service name from docker-compose
-    'database': 'local',
-}
-def get_db_connection():
-    connection = mysql.connector.connect(**db_config)
-    cursor = connection.cursor(dictionary=True)
-    return connection, cursor
-
-def get_data():
-    conn, cursor = get_db_connection()
-    cursor.execute('SELECT * FROM ttable')
-    rows = cursor.fetchall()
-    cursor.close()
-    conn.close()
-    messages = [{"id":row['id'], "author": row['author'], "message":row['message']} for row in rows]
-    return messages
-
-def insert_data(author, message):
-    conn, cursor = get_db_connection()
-    cursor.execute(f'INSERT INTO ttable (`author`, `message`) VALUES ("{author}", "{message}")')
-    conn.commit()
-    conn.close()
 
 def get_hit_count():
     retries = 5
@@ -53,19 +27,3 @@ def hello():
     greeting = 'Hello World, this is the {} times refreshed.'.format(count)
     return render_template('homepage.html', content=greeting) 
 
-@app.route('/messages')
-def show_messages():
-    messages = get_data()
-    return render_template('messages.html', messages=messages)
-
-@app.route('/messages/<name>')
-def chat(name):
-    messages = get_data()
-    return render_template('send_message.html',author=name, messages=messages) 
-
-@app.route('/send_message')
-def send_message():
-    author = request.args.get('author')
-    message= request.args.get('message')
-    insert_data(author, message)
-    return redirect(f"/messages/{author}") 
---
+++ app/messages/routes.py
@@ -0,0 +1,51 @@
+from flask import Blueprint
+from flask import Flask, render_template, request, redirect
+
+# Prefix /messages
+mess_bp = Blueprint('messages', __name__)
+
+# mySQL
+import mysql.connector
+db_config = {
+    'user': 'root',
+    'password': 'root',
+    'host': 'mysql',  # Service name from docker-compose
+    'database': 'local',
+}
+def get_db_connection():
+    connection = mysql.connector.connect(**db_config)
+    cursor = connection.cursor(dictionary=True)
+    return connection, cursor
+
+@mess_bp.route('/api')
+def get_data():
+    conn, cursor = get_db_connection()
+    cursor.execute('SELECT * FROM ttable')
+    rows = cursor.fetchall()
+    cursor.close()
+    conn.close()
+    messages = [{"id":row['id'], "author": row['author'], "message":row['message']} for row in rows]
+    return messages
+
+def insert_data(author, message):
+    conn, cursor = get_db_connection()
+    cursor.execute(f'INSERT INTO ttable (`author`, `message`) VALUES ("{author}", "{message}")')
+    conn.commit()
+    conn.close()
+
+@mess_bp.route('/')
+def show_messages():
+    messages = get_data()
+    return render_template('messages.html', messages=messages)
+
+@mess_bp.route('/<name>')
+def chat(name):
+    messages = get_data()
+    return render_template('send_message.html',author=name, messages=messages) 
+
+@mess_bp.route('/send_message')
+def send_message():
+    author = request.args.get('author')
+    message= request.args.get('message')
+    insert_data(author, message)
+    return redirect(f"/messages/{author}") 
