572caedc09a6d06d1b44a076f0611671c2e1ba53
Ting-Wei, Cheng <r11922118@csie.ntu.edu.tw>
2024-08-21 20:54:43 +0800

    [feature]add send message

--- app/app.py
+++ app/app.py
@@ -1,8 +1,9 @@
 import time
 
 import redis
-from flask import Flask, render_template, jsonify
+from flask import Flask, render_template, request, redirect
 import os
+import time
 
 app = Flask(__name__)
 cache = redis.Redis(host='redis', port=6379)
@@ -17,17 +18,23 @@ db_config = {
 }
 def get_db_connection():
     connection = mysql.connector.connect(**db_config)
-    return connection
-    
+    cursor = connection.cursor(dictionary=True)
+    return connection, cursor
+
 def get_data():
-    conn = get_db_connection()
-    cursor = conn.cursor(dictionary=True)
-    cursor.execute('SELECT * FROM ttable')  # Replace 'your_table_name' with your actual table name
+    conn, cursor = get_db_connection()
+    cursor.execute('SELECT * FROM ttable')
     rows = cursor.fetchall()
     cursor.close()
     conn.close()
     messages = [{"id":row['id'], "author": row['author'], "message":row['message']} for row in rows]
     return messages
+
+def insert_data(author, message):
+    conn, cursor = get_db_connection()
+    cursor.execute(f'INSERT INTO ttable (`author`, `message`) VALUES ("{author}", "{message}")')
+    conn.commit()
+    conn.close()
 
 def get_hit_count():
     retries = 5
@@ -47,7 +54,18 @@ def hello():
     return render_template('homepage.html', content=greeting) 
 
 @app.route('/messages')
-def hello_app():
-    
+def show_messages():
     messages = get_data()
     return render_template('messages.html', messages=messages)
+
+@app.route('/messages/<name>')
+def chat(name):
+    messages = get_data()
+    return render_template('send_message.html',author=name, messages=messages) 
+
+@app.route('/send_message')
+def send_message():
+    author = request.args.get('author')
+    message= request.args.get('message')
+    insert_data(author, message)
+    return redirect(f"/messages/{author}") 
