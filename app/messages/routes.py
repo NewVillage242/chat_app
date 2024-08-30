9abb3e88d21fb758c8ad72ad6693ffb8d91066df
twei <tingwei109@gmail>
2024-08-30 18:51:05 +0800

    [redis] TODO: fix index bug

--- app/messages/routes.py
+++ app/messages/routes.py
@@ -1,8 +1,13 @@
 from flask import Blueprint
 from flask import Flask, render_template, request, redirect
+import redis
+import json
 
 # Prefix /messages
 mess_bp = Blueprint('messages', __name__)
+
+#redis
+cache = redis.Redis(host='redis', port=6379)
 
 # mySQL
 import mysql.connector
@@ -19,17 +24,26 @@ def get_db_connection():
 
 @mess_bp.route('/api')
 def get_data():
-    conn, cursor = get_db_connection()
-    cursor.execute('SELECT * FROM ttable')
-    rows = cursor.fetchall()
-    cursor.close()
-    conn.close()
-    messages = [{"id":row['id'], "author": row['author'], "message":row['message']} for row in rows]
+    if cache.get('chat-key'):
+        messages = json.loads(cache.get('chat-key')) 
+    else:
+        conn, cursor = get_db_connection()
+        cursor.execute('SELECT * FROM ttable')
+        rows = cursor.fetchall()
+        cursor.close()
+        conn.close()
+        messages = [{"id":row['id'], "author": row['author'], "message":row['message']} for row in rows]
+        cache.set('chat-key', json.dumps(messages))
     return messages
 
 def insert_data(author, message):
     conn, cursor = get_db_connection()
     cursor.execute(f'INSERT INTO ttable (`author`, `message`) VALUES ("{author}", "{message}")')
+    # update data inside redis
+    if cache.get('chat-key'):
+        dics = json.loads(cache.get('chat-key'))
+        dics.append({'id': 100, 'author': author, 'message':message})
+        cache.set('chat-key', json.dumps(dics))
     conn.commit()
     conn.close()
 
